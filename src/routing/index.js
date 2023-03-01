3ac1fa1c9a0824a5b044f1faf4c9877c683abd50
mbright90 <michael.bright90@icloud.com>
2023-03-01 09:59:35 +0000

    Set up RouteSwitch

---
+++ src/routing/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { HashRouter, Routes } from 'react-router-dom'
+
+const RouteSwitch = () => {
+  return (
+    <HashRouter>
+      <Routes></Routes>
+    </HashRouter>
+  )
+}
+
+export default RouteSwitch
