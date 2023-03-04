10953bd763b6f79ad5d420ad482cd2a455526554
mbright90 <michael.bright90@icloud.com>
2023-03-04 14:31:02 +0000

    Add search page routing

--- src/routing/index.js
+++ src/routing/index.js
@@ -1,6 +1,7 @@
 import App from '@components/app'
 import Homepage from '@components/homepage'
 import Header from '@components/nav/header'
+import SearchPageOverview from '@components/shop/searchPage/searchPageOverview'
 import React from 'react'
 import { HashRouter, Route, Routes } from 'react-router-dom'
 
@@ -11,6 +12,7 @@ const RouteSwitch = () => {
         <Header />
         <Routes>
           <Route path="/" element={<Homepage />} />
+          <Route path="/products" element={<SearchPageOverview />} />
         </Routes>
       </HashRouter>
     </App>
