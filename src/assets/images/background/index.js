81dd1bc816c7e822f2ea386ad9efa1b644d54f3a
mbright90 <michael.bright90@icloud.com>
2023-03-04 16:13:23 +0000

    Restructure image folder

---
+++ src/assets/images/background/index.js
@@ -0,0 +1,9 @@
+import gTwoProImage from '@assets/images/background/g2-pro.png'
+import dlrImage from '@assets/images/background/manson-dlr-pro.jpg'
+import mbProImage from '@assets/images/background/mb-hero-pro.jpg'
+import mbWhitePro from '@assets/images/background/mb-white-pro.png'
+import oryxProImage from '@assets/images/background/oryx-pro.jpg'
+
+const bgImages = { dlrImage, gTwoProImage, mbProImage, mbWhitePro, oryxProImage }
+
+export default bgImages
