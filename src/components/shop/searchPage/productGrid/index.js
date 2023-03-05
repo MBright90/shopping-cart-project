066da39ca2798126e70872b54394db24e914d886
mbright90 <michael.bright90@icloud.com>
2023-03-05 15:55:55 +0000

    Implement productCard array

--- src/components/shop/searchPage/productGrid/index.js
+++ src/components/shop/searchPage/productGrid/index.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import { productContext } from '@components/app/productContext'
+import ProductCard from '@components/shop/searchPage/productCard'
+import React, { useContext } from 'react'
 
 import style from './ProductGrid.module.scss'
 
 const ProductGrid = () => {
-  return <div className={style.productGrid}></div>
+  const { currentProducts } = useContext(productContext)
+
+  const productArray = currentProducts.map((product) => {
+    console.log(product)
+    return <ProductCard product={product} key={product.name} />
+  })
+  console.log(productArray)
+
+  return <div className={style.productGrid}>{productArray}</div>
 }
 
 export default ProductGrid
