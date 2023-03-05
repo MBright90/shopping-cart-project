ae5b2082ecb7cd386066f0868aebedfa7d9e4ea2
mbright90 <michael.bright90@icloud.com>
2023-03-05 11:57:42 +0000

    Add component test

---
+++ src/components/app/productContext/ProductContext.test.js
@@ -0,0 +1,53 @@
+import { render, screen } from '@testing-library/react'
+import React, { useContext } from 'react'
+
+import ProductContextProvider, { productContext } from '.'
+
+const TestComponent = () => {
+  const { productDatabase } = useContext(productContext)
+
+  return (
+    <div>
+      {productDatabase.guitars.map((guitar) => {
+        ;<div key={guitar.name}>
+          <h2>{guitar.name}</h2>
+          <img src={guitar.image} alt={guitar.name} />
+          <p>{guitar.description}</p>
+          <p>£{guitar.price}</p>
+        </div>
+      })}
+    </div>
+  )
+}
+
+describe('productContext', () => {
+  it('renders to match snapshot', () => {
+    const { container } = render(
+      <ProductContextProvider>
+        <TestComponent />
+      </ProductContextProvider>
+    )
+
+    expect(container).toMatchSnapshot()
+  })
+
+  it('provides product data', () => {
+    render(
+      <ProductContextProvider>
+        <TestComponent />
+      </ProductContextProvider>
+    )
+
+    const maTwoName = screen.getByText(/MA-2/)
+    expect(maTwoName).toBeInTheDocument()
+
+    const maTwoImage = screen.getByAltText(/MA-2/)
+    expect(maTwoImage).toBeInTheDocument()
+
+    const maTwodescription = screen.getByText(/^The Manson MA-2/)
+    expect(maTwodescription).toBeInTheDocument()
+
+    const maTwoPrice = screen.getByText(/£949/)
+    expect(maTwoPrice).toBeInTheDocument()
+  })
+})
