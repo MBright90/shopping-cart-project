967d7e7414c5d72493832f211462efdf44965ebb
mbright90 <michael.bright90@icloud.com>
2023-03-07 16:56:00 +0000

    Add responsive styling and button disabling

--- src/components/utilities/inputQuantity/index.js
+++ src/components/utilities/inputQuantity/index.js
@@ -1,33 +1,57 @@
-import React from 'react'
+import { number } from 'prop-types'
+import React, { useState } from 'react'
 
 import style from './InputQuantity.module.scss'
 
-const InputQuantity = () => {
+const InputQuantity = ({ orderQuantity }) => {
+  const [quantity, setQuantity] = useState(orderQuantity)
+
   const handlePlusClick = () => {
-    console.log('minus')
+    if (quantity < 9) setQuantity(quantity + 1)
   }
 
   const handleMinusClick = () => {
-    console.log('minus')
+    if (quantity > 1) setQuantity(quantity - 1)
   }
 
   return (
     <div className={style.inputQuantity}>
-      <button className={style.quantityButton} type="button" onClick={handlePlusClick}>
-        +
+      <button
+        className={style.quantityButton}
+        type="button"
+        onClick={handleMinusClick}
+        disabled={quantity <= 1}
+      >
+        -
       </button>
       <input
         className={style.quantityInput}
         type="number"
         id="quantity-input"
         data-testid="quantity-input"
+        onKeyDown="return false"
+        value={quantity}
         min={0}
+        max={9}
       />
-      <button className={style.quantityButton} type="button" onClick={handleMinusClick}>
-        -
+      <button
+        className={style.quantityButton}
+        type="button"
+        onClick={handlePlusClick}
+        disabled={quantity >= 9}
+      >
+        +
       </button>
     </div>
   )
+}
+
+InputQuantity.defaultProps = {
+  orderQuantity: 1
+}
+
+InputQuantity.propTypes = {
+  orderQuantity: number.isRequired
 }
 
 export default InputQuantity
