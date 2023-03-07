aee02b947b2b9acc6934db3394bfdcf5e0d88fa3
mbright90 <michael.bright90@icloud.com>
2023-03-07 09:40:54 +0000

    Add changeCategory functionality

--- src/components/shop/searchPage/searchPageOverview/index.js
+++ src/components/shop/searchPage/searchPageOverview/index.js
@@ -1,10 +1,17 @@
+import { productContext } from '@components/app/productContext'
 import FilterBar from '@components/shop/searchPage/filterBar'
 import ProductGrid from '@components/shop/searchPage/productGrid'
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 
 import style from './SearchPageOverview.module.scss'
 
 const SearchPageOverview = () => {
+  const { changeCategory } = useContext(productContext)
+
+  // Include dependency array for successful category reset on unmounting
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(() => () => changeCategory('all'), [])
+
   return (
     <main className={style.searchPageOverview}>
       <FilterBar />
