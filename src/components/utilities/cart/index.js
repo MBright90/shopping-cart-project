8d5249fee9b7df0b86a9dff718402a5268657c9e
mbright90 <michael.bright90@icloud.com>
2023-03-08 17:13:04 +0000

    Structure cart containers

--- src/components/utilities/cart/index.js
+++ src/components/utilities/cart/index.js
@@ -4,9 +4,25 @@ import React, { useContext } from 'react'
 import style from './Cart.module.scss'
 
 const Cart = () => {
-  const { cartContents, setIsShowing } = useContext(cartContext)
+  const { cartContents, isShowing, setIsShowing } = useContext(cartContext)
+  console.log(cartContents)
 
-  return()
+  return (
+    <div>
+      <div
+        className={`${style.cartOverlay} ${isShowing ? style.overlayShowing : null}`}
+        onClick={() => {
+          setIsShowing(false)
+        }}
+        onKeyDown={(e) => {
+          if (e.code === 'Enter') setIsShowing(false)
+        }}
+      ></div>
+      <div className={`${style.cart} ${isShowing ? style.cartShowing : null}`}>
+        <h1 className={style.cartTitle}>Your Cart</h1>
+      </div>
+    </div>
+  )
 }
 
 export default Cart
