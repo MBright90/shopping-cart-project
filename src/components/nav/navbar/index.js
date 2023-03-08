b5c52ff3287a229b2e77177a36cfca12a1539685
mbright90 <michael.bright90@icloud.com>
2023-03-08 11:50:06 +0000

    Add cart contents amount and styling to navbar

--- src/components/nav/navbar/index.js
+++ src/components/nav/navbar/index.js
@@ -1,11 +1,14 @@
+import { cartContext } from '@components/app/cartContext'
 import NavItem from '@components/nav/navItem'
 import { faBasketShopping } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React from 'react'
+import React, { useContext } from 'react'
 
 import style from './Navbar.module.scss'
 
 const Navbar = () => {
+  const { cartContents } = useContext(cartContext)
+
   const navArray = ['Home', 'Products', 'About']
   const navItemArray = navArray.map((listItem) => (
     <NavItem LinkDestination={listItem} key={listItem} />
@@ -14,8 +17,10 @@ const Navbar = () => {
   return (
     <div className={style.navbar}>
       <ul className={style.linkList}>{navItemArray}</ul>
+
       <div className={style.basketLink}>
         <FontAwesomeIcon icon={faBasketShopping} />
+        <p style={{ marginLeft: '3px', fontWeight: 'bold' }}>{cartContents.length}</p>
       </div>
     </div>
   )
