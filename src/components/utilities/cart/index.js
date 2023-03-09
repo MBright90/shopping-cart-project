377c69f22aa72651aff6d15e740d7a9c1811c2f1
mbright90 <michael.bright90@icloud.com>
2023-03-09 11:38:19 +0000

    Add fixed postion and prop passing

--- src/components/utilities/cart/index.js
+++ src/components/utilities/cart/index.js
@@ -2,10 +2,15 @@ import { cartContext } from '@components/app/cartContext'
 import React, { useContext } from 'react'
 
 import style from './Cart.module.scss'
+import CartCheckout from './cartCheckout'
+import CartProduct from './cartProduct'
 
 const Cart = () => {
-  const { cartContents, isShowing, setIsShowing } = useContext(cartContext)
-  console.log(cartContents)
+  const { cartContents, isShowing, setIsShowing, totalPrice } = useContext(cartContext)
+
+  const removeProduct = () => {
+    console.log('remove me')
+  }
 
   return (
     <div>
@@ -20,6 +25,20 @@ const Cart = () => {
       ></div>
       <div className={`${style.cart} ${isShowing ? style.cartShowing : null}`}>
         <h1 className={style.cartTitle}>Your Cart</h1>
+        {cartContents.length <= 0 ? (
+          <div className={style.emptyCart}>
+            <p>You have no items in your cart</p>
+          </div>
+        ) : (
+          <React.Fragment>
+            <div className={style.cartProducts}>
+              {cartContents.map((product) => (
+                <CartProduct product={product} removeProduct={removeProduct} key={product.id} />
+              ))}{' '}
+            </div>
+            <CartCheckout total={totalPrice} />
+          </React.Fragment>
+        )}
       </div>
     </div>
   )
