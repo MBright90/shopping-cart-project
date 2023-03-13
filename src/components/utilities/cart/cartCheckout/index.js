57262b5002dc0001eb1aa21aecc2caa766156a2c
mbright90 <michael.bright90@icloud.com>
2023-03-13 16:39:31 +0000

    Add state for immediate updating of price

--- src/components/utilities/cart/cartCheckout/index.js
+++ src/components/utilities/cart/cartCheckout/index.js
@@ -1,12 +1,18 @@
 import { number } from 'prop-types'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import style from './CartCheckout.module.scss'
 
 const CartCheckout = ({ totalPrice }) => {
+  const [formattedPrice, setFormattedPrice] = useState('0.00')
+
+  useEffect(() => {
+    setFormattedPrice((Math.floor(totalPrice * 100) / 100).toFixed(2))
+  }, [totalPrice])
+
   return (
     <div className={style.cartCheckoutContainer}>
-      <div className={style.totalPrice}>£{totalPrice}</div>
+      <div className={style.totalPrice}>£{formattedPrice}</div>
       <button className={style.checkoutButton}>Checkout</button>
     </div>
   )
