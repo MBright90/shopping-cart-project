88e1690c32a3d3d00cb5b2acdf825bfd8c5de5b4
mbright90 <michael.bright90@icloud.com>
2023-03-14 16:39:52 +0000

    Add AboutPage tests

---
+++ src/components/aboutPage/AboutPage.test.js
@@ -0,0 +1,17 @@
+import { render } from '@testing-library/react'
+import React from 'react'
+
+import AboutPage from './index'
+
+describe('HomepageImage', () => {
+  it('renders to match snapshot', () => {
+    const { container } = render(<AboutPage />)
+    expect(container).toMatchSnapshot()
+  })
+
+  it('renders the a tag with the correct href', () => {
+    render(<AboutPage />)
+    const aLink = screen.getByText(/mBright90$/i)
+    expect(aLink).toHaveAttribute('href', 'https://github.com/MBright90/shopping-cart-project')
+  })
+})
