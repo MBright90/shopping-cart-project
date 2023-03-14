52e0895278fcf66a95262c72ffae80209dacaffd
mbright90 <michael.bright90@icloud.com>
2023-03-14 17:13:13 +0000

    Add context testing

---
+++ src/components/app/cartContext/CartContext.test.js
@@ -0,0 +1,105 @@
+import { render, userEvent } from '@testing-library/react'
+import React, { useContext } from 'react'
+
+import CartContextProvider, { cartContext } from '.'
+
+describe('CartContextProvider', () => {
+  it('adds a product to cart', () => {
+    const TestComponent = () => {
+      const { addProductToCart } = useContext(cartContext)
+      const product = { id: 1, name: 'Test Product', price: 10.0 }
+      return <button onClick={() => addProductToCart(product, 1)}>Add to Cart</button>
+    }
+
+    render(
+      <CartContextProvider>
+        <TestComponent />
+      </CartContextProvider>
+    )
+
+    const addProductButton = screen.getByText(/Add to Cart/i)
+    userEvent.click(addProductButton)
+    expect(screen.getByText('Cart (1)')).toBeInTheDocument()
+  })
+
+  it('removes a product from cart', () => {
+    const TestComponent = () => {
+      const { addProductToCart, removeProductFromCart } = useContext(cartContext)
+      const product = { id: 1, name: 'Test Product', price: 10.0 }
+      return (
+        <>
+          <button onClick={() => addProductToCart(product, 1)}>Add to Cart</button>
+          <button onClick={() => removeProductFromCart(1)}>Remove from Cart</button>
+        </>
+      )
+    }
+
+    render(
+      <CartContextProvider>
+        <TestComponent />
+      </CartContextProvider>
+    )
+
+    const addProductButton = screen.getByText(/Add to Cart/i)
+    userEvent.click(addProductButton)
+    expect(screen.getByText('Cart (1)')).toBeInTheDocument()
+
+    const removeProductButton = screen.getByText(/Remove from Cart/i)
+    userEvent.click(removeProductButton)
+    expect(screen.queryByText('Cart (1)')).not.toBeInTheDocument()
+  })
+
+  it('updates product quantity in cart', () => {
+    const TestComponent = () => {
+      const { addProductToCart, updateCartQuantity } = React.useContext(cartContext)
+      const product = { id: 1, name: 'Test Product', price: 10.0 }
+      return (
+        <>
+          <button onClick={() => addProductToCart(product, 1)}>Add to Cart</button>
+          <button onClick={() => updateCartQuantity(1, 2)}>Update Quantity</button>
+        </>
+      )
+    }
+
+    render(
+      <CartContextProvider>
+        <TestComponent />
+      </CartContextProvider>
+    )
+
+    const addProductButton = screen.getByText(/Add to Cart/i)
+    userEvent.click(addProductButton)
+    expect(screen.getByText('Cart (1)')).toBeInTheDocument()
+
+    const updateQuantityButton = screen.getByText(/Update Quantity/i)
+    userEvent.click(updateQuantityButton)
+    expect(screen.getByText('Cart (2)')).toBeInTheDocument()
+  })
+
+  test('applies discount code', () => {
+    const TestComponent = () => {
+      const { applyDiscountCode, activeDiscount } = React.useContext(cartContext)
+      return (
+        <>
+          <div>Active Discount: {activeDiscount * 100}%</div>
+          <button onClick={() => applyDiscountCode('mbright90')}>Apply Discount</button>
+        </>
+      )
+    }
+
+    render(
+      <CartContextProvider>
+        <TestComponent />
+      </CartContextProvider>
+    )
+
+    const beforeDiscount = screen.getByText(/Active Discount: 0%/i)
+    expect(beforeDiscount).toBeInTheDocument()
+
+    const applyDiscountButton = screen.getByText(/Apply Discount/i)
+    userEvent.click(applyDiscountButton)
+
+    const afterDiscount = screen.getByText(/Active Discount: 10%/i)
+    expect(afterDiscount).toBeInTheDocument()
+  })
+})
---
+++ src/components/app/modalContext/ModalContext.test.js
@@ -0,0 +1,42 @@
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import React, { useContext } from 'react'
+
+import ModalContextProvider, { modalContext } from './index'
+
+describe('ModalContextProvider', () => {
+  it('matches a snapshot when rendering', () => {
+    render(
+      <ModalContextProvider>
+        <div>Test Div</div>
+      </ModalContextProvider>
+    )
+
+    const div = screen.getByText(/Test Div/i)
+    expect(div).toMatchSnapshot()
+  })
+
+  it('provides currentModal and addNewModal values', () => {
+    const TestComponent = () => {
+      const { currentModal, addNewModal } = useContext(modalContext)
+      return (
+        <>
+          <div>{currentModal}</div>
+          <button onClick={() => addNewModal('Test Modal')}>Add Modal</button>
+        </>
+      )
+    }
+
+    render(
+      <ModalContextProvider>
+        <TestComponent />
+      </ModalContextProvider>
+    )
+
+    const modalButton = screen.getByText(/Add Modal/i)
+    userEvent.click(modalButton)
+
+    const testModalDisplay = screen.getByText(/Test Modal/i)
+    expect(testModalDisplay).toBeInTheDocument()
+  })
+})
