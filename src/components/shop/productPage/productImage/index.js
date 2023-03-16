38a08046d8258a2979df4951f1e580210dfce96f
mbright90 <michael.bright90@icloud.com>
2023-03-16 10:42:25 +0000

    Add role to element for clarity

--- src/components/shop/productPage/productImage/index.js
+++ src/components/shop/productPage/productImage/index.js
@@ -4,7 +4,9 @@ import React from 'react'
 import style from './ProductImage.module.scss'
 
 const ProductImage = ({ image }) => {
-  return <div className={style.productImage} style={{ backgroundImage: `url("${image}")` }} />
+  return (
+    <div className={style.productImage} style={{ backgroundImage: `url("${image}")` }} role="img" />
+  )
 }
 
 ProductImage.propTypes = {
