dac1bca991549362d82f8f394e844ca0923c54a6
mbright90 <michael.bright90@icloud.com>
2023-03-21 14:27:59 +0000

    Add snapshot tests

--- src/components/nav/logo/Logo.test.js
+++ src/components/nav/logo/Logo.test.js
@@ -3,6 +3,10 @@ import React from 'react'
 
 import Logo from './index'
 
+jest.mock('@assets/images/manson-logo-red.png', () => ({
+  default: 'test-image'
+}))
+
 describe('Logo', () => {
   it('renders to match snapshot', () => {
     const { container } = render(<Logo />)
