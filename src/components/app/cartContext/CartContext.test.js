876a4cfc6f278cef367d0344ca43d4179c6b1c4e
mbright90 <michael.bright90@icloud.com>
2023-03-22 10:57:55 +0000

    Alter cartContext test imports

--- src/components/app/cartContext/CartContext.test.js
+++ src/components/app/cartContext/CartContext.test.js
@@ -1,4 +1,5 @@
-import { render, screen, userEvent } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import React, { useContext } from 'react'
 
 import CartContextProvider, { cartContext } from './index'
